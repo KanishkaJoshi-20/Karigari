3de8f9912e74e4aee6d9db8c81b52e6ccc906fe7
KanishkaJoshi-20 <joshikanishka24605@gmail.com>
2026-05-04 08:15:36 +0530

    Update navbar, profile, and cart slice

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,13 +2,12 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, getUserProfile } from "../redux/slices/authSlice";
-import { clearCart } from "../redux/slices/cartSlice";
 import MyOrders from "./MyOrderPage";
 
 const Profile = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
+  const { user, loading } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (!user) {
@@ -20,11 +19,10 @@ const Profile = () => {
     if (user) {
       dispatch(getUserProfile());
     }
-  }, [dispatch, user]);
+  }, [dispatch]);
 
   const handleLogout = () => {
     dispatch(logout());
-    dispatch(clearCart());
     navigate("/login");
   };
 
